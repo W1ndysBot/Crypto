6799bb9e16b9c7b7884dd68dc0b70f59b51b43e1
W1ndys <w1ndys@outlook.com>
2024-08-04 21:58:54 +0800

    更新菜单

--- main.py
+++ main.py
@@ -10,15 +10,26 @@ sys.path.append(
 
 from app.api import send_group_msg, send_private_msg
 
+menu_message = """
+编码解码功能如下：
+1. b64d 解码base64
+2. b64e 编码base64
+"""
 
-async def handle_crypto_help_message(websocket, group_id, user_id):
+
+async def handle_crypto_help_message_group(websocket, group_id, user_id):
     await send_group_msg(
         websocket,
         group_id,
-        f"""[CQ:at,qq={user_id}]
-编码解码功能如下：
-1. b64decode 解码base64编码
-2. b64encode 编码base64编码""",
+        f"[CQ:at,qq={user_id}]" + menu_message,
+    )
+
+
+async def handle_crypto_help_message_private(websocket, user_id):
+    await send_private_msg(
+        websocket,
+        user_id,
+        menu_message,
     )
 
 
@@ -27,27 +38,30 @@ async def handle_crypto_group_message(websocket, msg):
         user_id = msg["user_id"]
         group_id = msg["group_id"]
         raw_message = msg["raw_message"]
-        role = msg["sender"]["role"]
-        message_id = int(msg["message_id"])
+        # role = msg["sender"]["role"]
+        # message_id = int(msg["message_id"])
 
         if raw_message == "编码解码":
-            await handle_crypto_help_message(websocket, group_id, user_id)
+            await handle_crypto_help_message_group(websocket, group_id, user_id)
             return
 
-        # base64 编解码
-        if raw_message.startswith("b64decode "):
-            encoded_message = raw_message[len("b64decode ") :]
+        # base64
+        # 解码
+        if raw_message.startswith("b64d "):
+            encoded_message = raw_message[len("b64d ") :]
             if re.match(r"^[A-Za-z0-9+/]+={0,2}$", encoded_message):
                 decoded_message = base64.b64decode(encoded_message).decode()
                 decoded_message = f"[CQ:at,qq={user_id}]解码结果如下\n{decoded_message}"
                 await send_group_msg(websocket, group_id, decoded_message)
             else:
                 await send_group_msg(websocket, group_id, "无效的base64编码")
-        elif raw_message.startswith("b64encode "):
-            decoded_message = raw_message[len("b64encode ") :]
+        # 编码
+        elif raw_message.startswith("b64e "):
+            decoded_message = raw_message[len("b64e ") :]
             encoded_message = base64.b64encode(decoded_message.encode()).decode()
             encoded_message = f"[CQ:at,qq={user_id}]编码结果如下\n{encoded_message}"
             await send_group_msg(websocket, group_id, encoded_message)
+
     except Exception as e:
         logging.error(f"处理编解码消息失败: {e}")
         return
@@ -58,14 +72,26 @@ async def handle_crypto_private_message(websocket, msg):
         user_id = msg["user_id"]
         raw_message = msg["raw_message"]
 
-        # base64 编解码
-        if raw_message.startswith("b64decode "):
-            encoded_message = raw_message[len("b64decode ") :]
+        if raw_message == "编码解码":
+            await handle_crypto_help_message_private(websocket, user_id)
+            return
+
+        # base64
+        # 解码
+        if raw_message.startswith("b64d "):
+            encoded_message = raw_message[len("b64d ") :]
             if re.match(r"^[A-Za-z0-9+/]+={0,2}$", encoded_message):
                 decoded_message = base64.b64decode(encoded_message).decode()
+                decoded_message = f"[CQ:at,qq={user_id}]解码结果如下\n{decoded_message}"
                 await send_private_msg(websocket, user_id, decoded_message)
             else:
                 await send_private_msg(websocket, user_id, "无效的base64编码")
+        # 编码
+        elif raw_message.startswith("b64e "):
+            decoded_message = raw_message[len("b64e ") :]
+            encoded_message = base64.b64encode(decoded_message.encode()).decode()
+            encoded_message = f"[CQ:at,qq={user_id}]编码结果如下\n{encoded_message}"
+            await send_private_msg(websocket, user_id, encoded_message)
 
     except Exception as e:
         logging.error(f"处理编解码消息失败: {e}")
